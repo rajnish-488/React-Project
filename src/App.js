ba518e41fd7f8f31d57197adcf57c6ae238c2a5c
Rajnish Singh <78335498+rajnish-488@users.noreply.github.com>
2021-10-23 08:47:28 +0530

    some modification

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         
 
 
-        <Route path="/home" >
+        <Route path="/" Exact >
           <JumboImage />
           <Holdercard />
         </Route>
--- src/component/jumboImage/jumboImage.js
+++ src/component/jumboImage/jumboImage.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './jumboImage.css';
-import image1 from '../images/img1.jpg';
-import image2 from '../images/img2.jpg';
-import image3 from '../images/img3.jpg';
-import image4 from '../images/img4.jpg';
+import image1 from '../images/jumbo3.jpg';
+import { Jumbotron, Button } from 'reactstrap';
 
 
 const jumboImage = () => {
    return (
-      <div>
-         <div className="jumboimage">
-            <div className="jimage">
-               <img src={image1} alt="nating " />
-            </div>
-         </div>
-      </div>
+      <div className="rrjumbo" style={{backgroundImage : "url("+ image1 + ")",
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'}}>
+      <div style={{ height: "120px" }}></div>
+      <Jumbotron>
+        <h1 className=" rrjumbotitle">Hello, world!</h1>
+        <p className="rrjumbocontent">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to fea is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        
+        <hr className="my-2" />
+        <div className="rrjumbobutton">
+        <button className="">Watch videos</button>
+        </div>
+      </Jumbotron>
+     
+    </div>
    )
 }
 
